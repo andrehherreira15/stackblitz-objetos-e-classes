abad596150800edaddc8fa2718a03c9d34d96713
andrehherreira15 <andre.herreira.oliveira@escola.pr.gov.br>
2026-07-08 11:54:15 -0300

    atividade 3 primeiro metodo

--- .js
+++ .js
@@ -15,28 +15,59 @@
 // let pessoa2 = new Pessoa('Jorge', 16);
 // pessoa2.apresentar();
 
-//2
+// //2
 
-class contaBancaria {
-  constructor(titular, saldo){
-    this.titular = titular;
-    this.saldo = saldo;
+// class contaBancaria {
+//   constructor(titular){
+//     this.titular = titular;
+//     this.saldo = 2000;
+//   }
+
+//   depositar(valor){
+//     if(valor > 1){
+//     console.log(`Seu saldo agora é ${this.saldo + valor}.`)
+//     this.saldo = this.saldo + valor
+//     } else {
+//       console.log('O valor mínimo de depósito é 1 real.')
+//     }
+//   }
+
+//   sacar(valor){
+//     if(valor < this.saldo){
+//     console.log(`Seu saldo agora é ${this.saldo - valor}.`);
+//     this.saldo = this.saldo - valor
+//     } else {
+//       console.log('Valor maior que o permitido.')
+//     }
+//   }
+
+//   mostrarSaldo(){
+//     console.log(`Titular: ${this.titular}\nSaldo: ${this.saldo}`)
+//   }
+// }
+
+// let contaBancariaAndre = new contaBancaria('André');
+// contaBancariaAndre.mostrarSaldo();
+// contaBancariaAndre.sacar(300);
+// contaBancariaAndre.depositar(800);
+// contaBancariaAndre.mostrarSaldo();
+
+//3
+class Produto {
+  constructor(nome, preco){
+    this.nome = nome;
+    this.preco = preco;
+    this.quantidade = 50;
   }
 
-  depositar(valor){
-    console.log(`Seu saldo agora é ${this.saldo + valor}.`)
+  adicionarEstoque(qtd){
+    if(qtd >=1 & Number.isInteger(qtd)){
+      console.log(`Seu estoque agora é de ${this.quantidade + qtd}.`)
+      this.quantidade = this.quantidade + qtd
+    } else {
+      console.log('Informe uma quantidade real.')
+    }
   }
 
-  sacar(valor){
-    console.log(console.log(`Seu saldo agora é ${this.saldo - valor}.`))
-  }
-
-  mostrarSaldo(){
-    console.log(`Titular: ${this.titular}\nSaldo: ${this.saldo}`)
-  }
+  
 }
-
-let contaBancaria = new contaBancaria();
-contaBancaria.mostrarSaldo(500);
-contaBancaria.sacar(300)
-contaBancaria.depositar(800)
